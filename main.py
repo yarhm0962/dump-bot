5e9461ea6c7e3c09511daf8ae464e18421ec0c9a
yarhm0962 <tahilvallejos@gmail.com>
2026-08-09 06:55:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 @app.route('/')
 def home(): return "✅ RblXLua Service Running"
 @app.route('/ping')
